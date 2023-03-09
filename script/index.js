ed489fe2d6cbe0ecbf78715fbaee1b7acf8e1562
Dmitriu <alchevck44@bk.ru>
2023-03-09 16:13:07 +0300

    added nojekyll

---
+++ script/index.js
@@ -0,0 +1,42 @@
+const buttonOpenPopUp = document.querySelector('.profile__userbutton');
+const profileform = document.querySelector('.popup');
+const closeModal = profileform.querySelector('.popup__close');
+ /* --------------Попап------------*/
+function showPopUp(){
+  profileform.classList.toggle('popup_opened');
+};
+buttonOpenPopUp.addEventListener('click' , showPopUp);
+
+function closePopUp(){
+  profileform.classList.remove('popup_opened');
+};
+closeModal.addEventListener ('click' , closePopUp);
+
+/*--------------------Для отправки формы------------*/
+const formProfile = document.querySelector('.popup__form');
+const nameInput =  formProfile.querySelector('.popup__imput-user_name');
+const jobInput = formProfile.querySelector('.popup__imput-user_job');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubTitle = document.querySelector('.profile__subtitle');
+
+function handleFormSubmit (event) {
+  event.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubTitle.textContent = jobInput.value;
+
+  closePopUp();
+}
+formProfile.addEventListener('submit', handleFormSubmit);
+
+
+
+
+/*-----------------------------лайки-------------
+const likesElement = document.querySelectorAll('.element__like');
+
+for(let i = 0;i < likesElement.length; i++){
+  likesElement[i].addEventListener('click',() =>{
+  likesElement[i].classList.toggle('element__like_active');
+  });
+}
+*/
